26f37dbbb15008a8c4af87be86bcb3de0c9a20c3
Daniela Mesalles <amesalles@tigo.com.gt>
2020-07-04 21:08:46 -0600

    api

---
+++ api/main.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from fastapi import FastAPI, Query
+
+from src.models.train import train
+from src.models.predict import predict
+
+app = FastAPI()
+
+
+@app.get('/')
+async def test():
+    return {'return':'hello wolrd'}
+
+@app.post('/train')
+async def train_model():
+    train()
+
+    return {'Result': 'model.pkl produced'}
+
+
+@app.get('/predict')
+async def predict_review(sentences: List[str] = Query(..., description='Sentences to process')):
+    predictions = predict(sentences)
+
+    response = [
+        {
+            'id': idx + 1,
+            'sentence': sentence,
+            'prediction': sentiment
+        }
+        for idx, (sentence, sentiment) in enumerate(zip(sentences, predictions))
+    ]
+
+    return response
